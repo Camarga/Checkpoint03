2e4f83f7227d88846401f391da41df8ac8edb988
Camarga <77camarga@gmail.com>
2025-03-03 13:33:48 +0100

    Add files via upload

---
+++ checkpoint03assignment.py
@@ -0,0 +1,73 @@
+#Exercise 1
+street_name = 'Down'
+number = 21
+comerce = ['bakery', 'clothes', 'supermarket']
+city = True
+
+print(street_name)
+print(number)
+print(comerce)
+print(city)
+
+#Exercise 2
+
+street_name = 'Down'
+second_street_name = street_name[0:3]
+new_street_name = second_street_name
+
+print(new_street_name)
+
+#Exercise 3
+
+comerce = ['bakery', 'clothes', 'supermarket']
+print(comerce[0])
+
+#Exercise 4
+
+number = 21
+number_two = 10
+total = number + number_two
+
+print(total)
+
+#Exercise 5
+
+comerce = ['bakery', 'clothes', 'supermarket']
+last_comerce = comerce[-1]
+
+print(last_comerce)
+
+#Exercise 6
+
+names = 'harry,alex,susie,jared,gail,conner'
+list_of_names = names.split(',')
+
+print(list_of_names)
+
+#Exercise 7
+
+ability = 'super strength, super speed, heat vision'
+first_word = ability[0:5].upper()
+new_ability_sentence = first_word + ability[5:]
+
+print(new_ability_sentence)
+
+#Exercise 8
+
+number = 21
+age = f'I am {number} years old'
+
+print(age)  
+
+#Exercise 9
+
+greeting = 'hello world'
+
+print(greeting)
+
+#Segundo ejercicio
+
+saludo = 'Hola, estoy aprendiendo mucho'
+saludo = saludo.replace('Hola', 'Adios')
+
+print(saludo)
